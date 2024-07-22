6df9fdbd3dc0accae596b949bee445fe1c5ff0c4
somanth1d <sdeychowdhury7@gmail.com>
2024-07-22 12:24:00 +0530

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductContainerComponent } from './product-container/product-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ProductContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/product-container/product-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-container.component.html',
+  styleUrl: './product-container.component.css'
+})
+export class ProductContainerComponent {
+
+}
